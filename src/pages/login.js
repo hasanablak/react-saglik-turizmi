0a2156d24633233384e237b2023bcec7f4e2ae43
hasanablak <0hasanablak@gmail.com>
2023-01-12 16:03:52 +0300

    Kullanıcının giriş yapabilmesi için gerekli işlemleri sağladım

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,21 +1,15 @@
 import { Link, useNavigate, useSearchParams } from "react-router-dom"
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { useDispatch } from "react-redux";
 import { setAuth } from "../stores/auth";
 import axios from "axios";
 
 export default function Login() {
 
-	const [credenditinal, setCredentinal] = useState({email:"", password:""});
-	const [searchParams] = useSearchParams();
+	const [credenditinal, setCredentinal] = useState({ email: "", password: "" });
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
-	useEffect(() => {
-		if (searchParams.get("email")) {
-			setCredentinal(f => ({ ...f, email: searchParams.get("email") }));
-		}
-	}, [searchParams.get("email")]);
 
 	const sendLoginForm = async (e) => {
 		e.preventDefault();
@@ -27,51 +21,51 @@ export default function Login() {
 
 	}
 
-	const loginWasSuccessfully = async (e)  => {
+	const loginWasSuccessfully = async (e) => {
 		await dispatch(setAuth(e.data));
-		navigate("/dashboard");
+		navigate("/travellers");
 	}
 
 	return (
 		<div className="row d-flex justify-content-center align-items-center h-100">
-				<div className="col col-xl-8">
-					<div className="card  justify-content-center align-items-center">
-						<div className="card-body p-5">
-							<h6 className="mb-3 header">Login</h6>
-							<div className="card justify-content-center align-items-center p-3 w-100">
-								<form className="d-flex mb-4 flex-column" onSubmit={sendLoginForm}>
-									<div className="form-outline flex-fill mt-3">
-										<label className="form-label" htmlFor="email">E-mail</label>
-										<input
-											required 
-											pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
-											type="text"
-											id="email"
-											onChange={(v) => setCredentinal(f => ({ ...f, email: v.target.value }))}
-											className="form-control form-control-lg" />
-									</div>
-									<div className="form-outline flex-fill mt-3">
-										<label className="form-label" htmlFor="password">Password</label>
-										<input
-											required
-											type="password"
-											id="password"
-											onChange={(v) => setCredentinal(f => ({ ...f, password: v.target.value }))}
-											className="form-control form-control-lg" />
-									</div>
-									<div className="form-outline flex-fill my-3 mt-3">
-										<input
+			<div className="col col-xl-8">
+				<div className="card  justify-content-center align-items-center">
+					<div className="card-body p-5">
+						<h6 className="mb-3 header">Login</h6>
+						<div className="card justify-content-center align-items-center p-3 w-100">
+							<form className="d-flex mb-4 flex-column" onSubmit={sendLoginForm}>
+								<div className="form-outline flex-fill mt-3">
+									<label className="form-label" htmlFor="email">E-mail</label>
+									<input
+										required
+										pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
+										type="text"
+										id="email"
+										onChange={(v) => setCredentinal(f => ({ ...f, email: v.target.value }))}
+										className="form-control form-control-lg" />
+								</div>
+								<div className="form-outline flex-fill mt-3">
+									<label className="form-label" htmlFor="password">Password</label>
+									<input
+										required
+										type="password"
+										id="password"
+										onChange={(v) => setCredentinal(f => ({ ...f, password: v.target.value }))}
+										className="form-control form-control-lg" />
+								</div>
+								<div className="form-outline flex-fill my-3 mt-3">
+									<input
 										type="submit"
 										className="form-control form-control-md"
 										value="Login" />
-									</div>
-								</form>
-							</div>
-
+								</div>
+							</form>
 						</div>
-					</div>
 
+					</div>
 				</div>
+
 			</div>
+		</div>
 	);
 }
