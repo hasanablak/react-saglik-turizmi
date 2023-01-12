caefa4361462fd58d4ae22e58f5fd5c08c824180
hasanablak <0hasanablak@gmail.com>
2023-01-12 16:03:37 +0300

    AxiosError'u layout'da kullandım

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,6 @@
+import { AxiosError } from "../helpers/axios-error"
 export default function Layout({ children }) {
+	AxiosError();
 	return (
 		<section className="vh-75">
 			<div className="container h-100 mt-5 p-3 dashed" >
