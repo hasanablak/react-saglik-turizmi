f3c3936d4fa35ccd4231df8c82400ad8b386388e
hasanablak <0hasanablak@gmail.com>
2023-01-16 10:39:58 +0300

    menülerde gezinmeyi yaptım

--- src/layouts/auth-layout.js
+++ src/layouts/auth-layout.js
@@ -1,14 +1,19 @@
 import { BtnLogout } from '../components';
+import { useNavigate, useLocation, } from 'react-router-dom';
 export default function AuthLayout({ children }) {
+	const navigate = useNavigate();
+	const location = useLocation();
 	return (<div className="row">
 		<div className="col-md-2">
 			<div className="card mt-2 text-center">
 				<label>Menü</label>
-				<ul className="list-group">
-					<li className="list-group-item active">Yolcular</li>
-					<li className="list-group-item">Transferler</li>
-					<li className="list-group-item">Arabalar</li>
-					<li className="list-group-item">Kullanıcılar</li>
+				<ul className="list-group menu">
+					<li className={`list-group-item ${location.pathname == '/travellers' && 'active'}`} onClick={() => navigate('/travellers')}>
+						Yolcular
+					</li>
+					<li className={`list-group-item ${location.pathname == '/transports' && 'active'}`} onClick={() => navigate('/transports')}>Transferler</li>
+					<li className={`list-group-item ${location.pathname == '/cars' && 'active'}`} onClick={() => navigate('/cars')}>Arabalar</li>
+					<li className={`list-group-item ${location.pathname == '/users' && 'active'}`} onClick={() => navigate('/users')}>Kullanıcılar</li>
 				</ul>
 				<BtnLogout />
 			</div>
